b1f1ddf9fefa015e8e15f0629d99d05d0e1b494e
Intissar BENAZIZ <Intissar BENAZIZ@DESKTOP-RRJUMKP>
2023-11-05 12:33:35 +0100

    template exercice 1&4 tp4

---
+++ src/main/java/rbt/emsi/benaziz/tpc/tp2benaziz/jsf/Bean2.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package rbt.emsi.benaziz.tpc.tp2benaziz.jsf;
+
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author Intissar BENAZIZ
+ *
+ * backing bean pour stocker la propriété correspondant au champ du formulaire
+ * ce bean est creer pour gerer le form 4 DU TP4
+ */
+@Named(value = "bean2")
+@RequestScoped
+public class Bean2 {
+
+    private int nombre;
+    
+    public Bean2(){
+        
+    }
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Integer> getNombresSuivants() {
+        int nb = 5;
+        List<Integer> l = new ArrayList<>(nb);
+        for (int i = nombre; i < nombre + nb; i++) {
+            l.add(i);
+        }
+        return l;
+    }
+
+    public String afficher() {
+        return "affichage__4?nb=" + nombre + "&amp;faces-redirect=true";
+    }
+
+}
